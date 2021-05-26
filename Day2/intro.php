c4393d45280e998d7f921d4c5631a0277949f57d
Anjali303 <68639648+Anjali303@users.noreply.github.com>
2021-05-26 22:56:09 +0530

    Add files via upload

---
+++ Day2/intro.php
@@ -0,0 +1,50 @@
+<?php ?>
+<html>
+<body>
+ <h1>Welcome to my World!!</h1>
+
+<?php 
+	echo 'HI!! I am Anjali.</br></br>';
+	echo 'Doing PHP code.</br></br>';
+?>
+<style>
+table,th,td{
+	border: 1px solid black;
+	border-collapse : collapse;
+
+}
+th,td{
+	padding: 15px;
+	
+}
+table{
+	width : 50%;
+}
+</style>
+<table>
+<tr>
+<th colspan = '3' text-align : 'center'>Information</th>
+</tr>
+<tr>
+<th>Name</th>
+<th>Age</th>
+<th>Subject</th>
+</tr>
+<tr>
+<td>Anjali Patel</td>
+<td>21</td>
+<td>PHP</td>
+</tr>
+<tr>
+<td>Jinal Parmar</td>
+<td>21</td>
+<td>Python</td>
+</tr>
+<tr>
+<td>Vaidehi Pandya</td>
+<td>21</td>
+<td>JAVA</td>
+</tr>
+</table>
+</body>
+</html>
